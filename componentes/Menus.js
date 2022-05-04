33358352c2cb3a510497c008510b0aefbb3614cf
crepes <camilo.gonzalez@crepesywaffles.com>
2022-05-04 17:14:32 -0500

    cambio de imagenes

--- componentes/Menus.js
+++ componentes/Menus.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-import { mexicomenu, ecuadormenu, chilemenu, españamenu, panamamenu } from "../public/menus/index"
+import { mexicomenu, ecuadormenu, chilemenu, españamenu, panamamenu ,panamaimagen,colombiaimagen,mexicoimagen,españaimagen,ecuadorimagen,chileimagen} from "../public/menus/index"
 import Image from 'next/image'
 
 export default function Menus() {
@@ -9,19 +9,22 @@ export default function Menus() {
             <h2 className="title-menu mt-5">Menús</h2>
         <div className="d-flex justify-content-center flex-wrap">
           <div className="img-menu">
-            <Link href="https://online.flippingbook.com/view/1007065914/" ><a target="_blank"><Image width="230" height="250" src={mexicomenu}></Image></a></Link>
+            <Link href="https://online.flippingbook.com/view/1007065914/" ><a target="_blank"><Image width="200" height="200" src={mexicoimagen}></Image></a></Link>
           </div>
           <div className="img-menu">
-            <Link href="https://globalreports.info/wolop/Aserlaco/1/1/1/1" ><a target="_blank"><Image width="230" height="250" src={ecuadormenu}></Image></a></Link>
+            <Link href="https://globalreports.info/wolop/Aserlaco/1/1/1/1" ><a target="_blank"><Image width="200" height="200" src={ecuadorimagen}></Image></a></Link>
           </div>
           <div className="img-menu" >
-            <Link href="https://fiweex.com/sistema/img/pdf/carta%20QR%20FINAL%2003%20SEP%202020-compressed.pdf" ><a target="_blank"><Image width="230" height="250" src={chilemenu}></Image></a></Link>
+            <Link href="https://fiweex.com/sistema/img/pdf/carta%20QR%20FINAL%2003%20SEP%202020-compressed.pdf" ><a target="_blank"><Image width="200" height="200" src={chileimagen}></Image></a></Link>
           </div>
           <div className="img-menu">
-            <Link href="https://digital.adhocwifi.com/es/crepes/waffles/local"><a target="_blank"><Image width="230" height="250" src={españamenu}></Image></a></Link>
+            <Link href="https://digital.adhocwifi.com/es/crepes/waffles/local"><a target="_blank"><Image width="200" height="200" src={españaimagen}></Image></a></Link>
           </div>
           <div className="img-menu">
-            <Link href="https://qr.precompro.com/?vendor=crepespanama" target="_blank"><a target="_blank"><Image width="230" height="250" src={panamamenu}></Image></a></Link>
+            <Link href="https://qr.precompro.com/?vendor=crepespanama" target="_blank"><a target="_blank"><Image width="200" height="200" src={panamaimagen}></Image></a></Link>
+          </div>
+          <div className="img-menu">
+            <Link href="https://qr.precompro.com/?vendor=crepespanama" target="_blank"><a target="_blank"><Image width="200" height="200" src={colombiaimagen}></Image></a></Link>
           </div>
         </div>
         </div>
--- pages/Domicilios.js
+++ pages/Domicilios.js
@@ -6,6 +6,7 @@ import Image from 'next/image'
 import banner from '../public/banner_domicilios.jpg'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {españadomi,mexicodomi,chiledomi,ecuadordomi,panamadomi} from "../public/domicilios/index"
+import {panamaimagen,colombiaimagen,ecuadorimagen,mexicoimagen,españaimagen,chileimagen} from "../public/menus/index"
 
 export default function Domicilios() {
     return (
@@ -17,8 +18,10 @@ export default function Domicilios() {
             </div>
             <div className="container-domicilios">
                 <div className="img-box">
-                <Image src={mexicodomi} width="150" height="220" priority objectFit="cover" layout="responsive"/>
-                <h4>México</h4>
+                <Image src={mexicoimagen} width="200" height="180" priority objectFit="cover" layout="responsive"/>
+                {/* <h4>México</h4> */}
+                <br/>
+                
                 <a href="https://pedidos.crepesywaffles.mx/">pedidos.crepesywaffles.mx</a>
                 <h6>Rappi</h6>
                 <h6>Ubereats</h6>
@@ -27,8 +30,10 @@ export default function Domicilios() {
                 </div>
 
                 <div className="img-box">
-                <Image src={ecuadordomi} width="150" height="220" priority objectFit="cover" layout="responsive"/>
-                <h4>Ecuador</h4>
+                <Image src={ecuadorimagen} width="200" height="180" priority objectFit="cover" layout="responsive"/>
+                {/* <h4>Ecuador</h4> */}
+                <br/>
+                
                 <a href="https://pedidos.crepesywaffles.ec/">pedidos.crepesywaffles.ec</a>
                 <a href="https://api.whatsapp.com/send?phone=593991441185"  className="call-domi"><ion-icon name="logo-whatsapp"></ion-icon>+59 3991441185</a>
                 <a href="tel:+59326003420" className="call-domi"><ion-icon name="call-outline"></ion-icon>+59326003420</a>
@@ -38,21 +43,34 @@ export default function Domicilios() {
                 </div>
 
                 <div className="img-box">
-                <Image src={chiledomi} width="150" height="220" priority objectFit="cover" layout="responsive"/>
-                <h4>Chile</h4>
+                <Image src={chileimagen} width="200" height="180" priority objectFit="cover" layout="responsive"/>
+                {/* <h4>Chile</h4> */}
+                <br/>
                 <h6>Rappi</h6>
                 <h6>Ubereats</h6>
                 </div>
 
                 <div className="img-box">
-                <Image src={españadomi} width="150" height="220" priority objectFit="cover" layout="responsive"/>
-                <h4>España</h4>
+                <Image src={españaimagen} width="200" height="180" priority objectFit="cover" layout="responsive"/>
+                {/* <h4>España</h4> */}
+                <br/>
+                
                 <h6>Ubereats</h6>
                 </div>
 
                 <div className="img-box">
-                <Image src={panamadomi} width="150" height="220" priority objectFit="cover" layout="responsive"/>
-                <h4>Panamá</h4>
+                <Image src={panamaimagen} width="200" height="180" priority objectFit="cover" layout="responsive"/>
+                {/* <h4>Panamá</h4> */}
+                <br/>
+                
+                <h6>PedidosYa</h6>
+                <h6>Ubereats</h6>
+                </div>
+                <div className="img-box">
+                <Image src={colombiaimagen} width="200" height="180" priority objectFit="cover" layout="responsive"/>
+                {/* <h4>Panamá</h4> */}
+                <br/>
+                
                 <h6>PedidosYa</h6>
                 <h6>Ubereats</h6>
                 </div>
